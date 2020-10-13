a28ddbfa6aa8e9f79fea74d2328cc1d153094720
Travis B <tbabola@gmail.com>
2020-10-13 17:16:00 -0400

    Added HQ figure code

---
+++ SuppFigs_Videos/SV02_03_04_Pax2Cre_HD.m
@@ -0,0 +1,220 @@
+%%MRS2500 Video
+%video to use
+clear all; close all;
+fileList = loadFileList('.\Data\Pax2Cre\*45*.czi');
+imgdata = bfopen(fileList{1});
+imgdata = imgdata{1,1}; %this is where the imagedata is stored
+%%
+img = []; index = 1;
+parfor i = 1:2400
+    img(:,:,i) = imgdata{i,1};
+end
+clear imgdata;
+img = imresize(img,[NaN 480]);
+img = Kalman_Stack_Filter(img);
+%%
+%writeTif(single(mean(img(:,:,1:5),3)),'.\VidProcessing\firstFrameMRSandAnalysis.tif',32);
+%%
+leaderFrame = imread('.\Data\LeaderFrames\Pax2_MRS_leader.tif');
+%leaderFrame = leaderFrame(:,:,1:3);
+%leaderFrame
+%%
+name = ['.\Videos\SV03_Pax2_MRS2500_HD'];
+ext = '.mp4';
+fileOutput = [name ext];
+if strcmp(ext,'.avi')
+    v = VideoWriter(fileOutput,'Uncompressed AVI');
+elseif strcmp(ext,'.mp4')
+    v = VideoWriter(fileOutput,'MPEG-4');
+    v.Quality = 100;
+end
+v.FrameRate = 10;
+open(v);
+
+green = zeros(255,3);
+green(:,2) = [1:255]';
+green = green/255;
+
+
+oimg = []; index = 1; drugApp = 825;
+timernum = '0';
+figure; imagesc(zeros(480,480)); truesize; clims = [500 30000]; box off; hold on;
+for i = 560:2:1200
+    if i < 600
+        imagesc(leaderFrame);
+    elseif i >= 600
+        hold off;
+        imagesc(img(:,:,i)); colormap(green); caxis(clims); 
+        hold on;
+        if i >= drugApp
+            thandle = text(10, 40,'+MRS2500','FontSize',6,'Color','w','FontWeight','bold');
+        end
+        
+        if mod(i,2) == 0 
+          timernum = num2str((i-600)/2);
+        end
+        timer = text(480-10,480-15,[timernum ' s'],'FontSize',6,'color','w','FontWeight','bold','HorizontalAlignment','right');
+
+        if mod(i,100) == 0
+            disp(i);
+        end
+    end
+     
+    if i >= 560
+        thandle0 = text(10,15,'P0\it Pax2-Cre;GCaMP3','FontSize',6,'color','w','FontWeight','bold');
+    end
+    writeVideo(v,getframe); 
+end
+
+close(v);
+
+
+%% might as well use same video for analysis video
+name = ['.\Videos\SV02_ISCIHCanalysis_HD'];
+ext = '.mp4';
+fileOutput = [name ext];
+if strcmp(ext,'.avi')
+    v = VideoWriter(fileOutput,'Uncompressed AVI');
+elseif strcmp(ext,'.mp4')
+    v = VideoWriter(fileOutput,'MPEG-4');
+    v.Quality = 100;
+end
+v.FrameRate = 10;
+open(v);
+
+green = zeros(255,3);
+green(:,2) = [1:255]';
+green = green/255;
+
+fileList = loadFileList('.\Data\Pax2Cre\*45*.mat')
+%%%img,labels,positiveIndices,IHClabelroi,IHClocation,name,clims,h
+h = load(fileList{1});
+IHCstruct = h.IHCstruct;
+h = load(fileList{2});
+ISCstruct = h.ISCstruct;
+labels = ISCstruct.labelRoi;
+positiveIndices = ISCstruct.posIndices*480/512;
+IHClabelroi = IHCstruct.thrroi;
+IHClocation = IHCstruct.IHCcenters*480/512;
+timernum = 0;
+figure; imagesc(zeros(480,480)); truesize; clims = [500 30000]; box off; hold on;
+for i = -20:1:300
+     if i <= 0
+        imagesc(leaderFrame); 
+        if exist('timer')
+            delete(timer)
+        end
+    else
+        indx_green = find(labels(i,:)>=1);
+            imagesc(img(:,:,i)); colormap(green); caxis(clims); hold on;
+            if ~isempty(indx_green)
+                for j = 1:size(indx_green,2)
+                     plot(positiveIndices(indx_green(j),1:2:end),positiveIndices(indx_green(j),2:2:end),'Color','w');
+                end
+            end
+
+            %%IHCs
+            indx_green = find(IHClabelroi(i,:)>=1);
+            if ~isempty(indx_green)
+                for j = 1:size(indx_green,2)
+                    temp = indx_green(j);
+                    drawellipse('Position',IHClocation(temp,:),'SemiAxes',[1 1],'Color','w','LineWidth',1,'InteractionsAllowed','none');
+                    %drawpoint(gca, 'Position',IHClocation(temp,:),'Color','w','MarkerSize',1,'LineWidth',0);
+                end
+            end          
+            
+
+            if mod(i,2) == 0 
+                timernum = num2str(i/2);
+            end
+
+            timer = text(480-10,480-15,[timernum ' s'],'FontSize',6,'color','w','FontWeight','bold','HorizontalAlignment','right');
+
+            drawnow;
+            
+            hold off;
+            if mod(i,100) == 0
+                disp(i);
+            end
+     end
+
+    thandle0 = text(10,15,'P0\it Pax2-Cre;GCaMP3','FontSize',6,'color','w','FontWeight','bold');
+    writeVideo(v,getframe); 
+end   
+
+close(v)
+%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%apex/base video
+%Image 41 and Image 39, same as figure
+clear all; close all;
+fileList = loadFileList('.\Data\Pax2Cre\*35*.czi');
+imgdata = bfopen(fileList{1});
+imgdata = imgdata{1,1}; %this is where the imagedata is stored
+img = []; index = 1;
+parfor i = 1:600
+    img(:,:,i) = imgdata{i,1};
+end
+clear imgdata;
+img = Kalman_Stack_Filter(img);
+%%
+fileList = loadFileList('.\Data\Pax2Cre\*41*.czi');
+imgdata = bfopen(fileList{1});
+imgdata = imgdata{1,1}; %this is where the imagedata is stored
+imgapex = []; index = 1;
+parfor i = 1:600
+    imgapex(:,:,i) = imgdata{i,1};
+end
+clear imgdata;
+imgapex = Kalman_Stack_Filter(imgapex);
+%%
+bigImg = [imgapex img];
+clear img imgapex
+bigImg = imresize(bigImg,[NaN 480]);
+%%
+%writeTif(single(mean(bigImg(:,:,1:5),3)),'.\VidProcessing\firstFrameBaseApex.tif',32)
+%%
+leaderFrame = imread('.\Data\LeaderFrames\Pax2_Base_Apex_leader.tif');
+%%
+name = ['.\Videos\SV04_ApexvsBase_HD'];
+ext = '.mp4';
+fileOutput = [name ext];
+if strcmp(ext,'.avi')
+    v = VideoWriter(fileOutput,'Uncompressed AVI');
+elseif strcmp(ext,'.mp4')
+    v = VideoWriter(fileOutput,'MPEG-4');
+    v.Quality = 100;
+end
+v.FrameRate = 10;
+open(v);
+
+green = zeros(255,3);
+green(:,2) = [1:255]';
+green = green/255;
+
+oimg = []; index = 1;
+timernum = 0;
+figure; imagesc(zeros(size(bigImg,1,2))); colormap(green); truesize; clims = [500 30000]; box off; hold on; axis off;
+for i = -20:1:600
+    if i < 1
+        imagesc(leaderFrame)
+    else
+        hold off; imagesc(bigImg(:,:,i)); caxis(clims); box off; axis off; hold on;
+        if mod(i,2) == 0 
+           timernum = num2str(i/2);
+        end
+
+        timer = text(480-5,240-15,[timernum ' s'],'FontSize',6,'color','w','FontWeight','bold','HorizontalAlignment','right');
+
+        if mod(i,100) == 0
+            disp(i);
+        end
+    end
+    thandle = text(10, 15,'P0 Apex','FontSize',6,'Color','w','FontWeight','bold');
+    thandle2 = text(240+10,15,'P0 Base','FontSize',6,'Color','w','FontWeight','bold');
+    writeVideo(v,getframe); 
+end
+
+close(v);
+
+
--- SuppFigs_Videos/SV08_SV10_Snap25_Dev_a9_MRS2500inj_HD.m
+++ SuppFigs_Videos/SV08_SV10_Snap25_Dev_a9_MRS2500inj_HD.m
@@ -8,14 +8,14 @@ leaderFrame = imread('.\Data\LeaderFrames\Developmental_Snap25_Stacked_leader.ti
 leaderFrame = imresize(leaderFrame,[NaN 480]);
 imagesc(leaderFrame)
 %%
-name = ['.\Videos\SV08_Snap25_IC_development_comp'];
+name = ['.\Videos\SV08_Snap25_IC_development_HD'];
 ext = '.mp4';
 fileOutput = [name ext];
 if strcmp(ext,'.avi')
     v = VideoWriter(fileOutput,'Uncompressed AVI');
 elseif strcmp(ext,'.mp4')
     v = VideoWriter(fileOutput,'MPEG-4');
-    v.Quality = 35;
+    v.Quality = 100;
 end
 v.FrameRate = 10;
 open(v);
@@ -33,11 +33,11 @@ for i = -40:2:600
     caxis(clims); 
     
     if i >= -40
-        thandle = text(10, 15,'P1','FontSize',16,'Color','w','FontWeight','bold');
-        thandle2 = text(10,162+15,'P7','FontSize',16,'Color','w','FontWeight','bold');
-        thandle3 = text(10,162*2+20,'P13','FontSize',16,'Color','w','FontWeight','bold');
+        thandle = text(10, 15,'P1','FontSize',6,'Color','w','FontWeight','bold');
+        thandle2 = text(10,162+15,'P7','FontSize',6,'Color','w','FontWeight','bold');
+        thandle3 = text(10,162*2+20,'P13','FontSize',6,'Color','w','FontWeight','bold');
         if i >= 1
-        thandle4 = text(5,480-5,'\it Snap25-T2A-GCaMP6s','FontSize',14,'Color','w','FontWeight','bold','HorizontalAlignment','left');
+        thandle4 = text(5,480-5,'\it Snap25-T2A-GCaMP6s','FontSize',5,'Color','w','FontWeight','bold','HorizontalAlignment','left');
         end
     end
     
@@ -45,7 +45,7 @@ for i = -40:2:600
         timernum = i/10;
     end
     if i > 1
-        timer = text(480-10,485-15,[num2str(timernum) ' s'],'FontSize',16,'color','w','FontWeight','bold','HorizontalAlignment','right');
+        timer = text(480-10,485-15,[num2str(timernum) ' s'],'FontSize',6,'color','w','FontWeight','bold','HorizontalAlignment','right');
     end
     
     if mod(i,100) == 0
@@ -66,7 +66,7 @@ leaderFrame = imread('.\Data\LeaderFrames\Snap25_a9_leader_2.tif');
 imagesc(leaderFrame)
 
 %%
-name = ['.\Videos\SV09_a9KO_GOF_HQ'];
+name = ['.\Videos\SV09_a9KO_GOF_HD'];
 ext = '.mp4';
 fileOutput = [name ext];
 if strcmp(ext,'.avi')
